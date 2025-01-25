525e04a1042eee515c01bef5deee64585e1e8fa8
TomDeBarra <Dylan.Barry@students.ittralee.ie>
2025-01-25 21:16:01 +0000

    Signed-off-by: TomDeBarra <Dylan.Barry@students.ittralee.ie>

--- AgeOfBattle/Assets/Scripts/Units/AbstractUnit.cs
+++ AgeOfBattle/Assets/Scripts/Units/AbstractUnit.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public abstract class AbstractUnit : MonoBehaviour
 {
     // Unit stats
-    private int health; // Health of the unit
-    private int damage; // Damage dealt by the unit
+    protected int health; // Health of the unit
+    protected int damage; // Damage dealt by the unit
 
     // Movement variables
-    private float speed; // Speed of the unit
-    private int direction = -1; // Direction of movement: 1 for positive x, -1 for negative x
+    protected float speed; // Speed of the unit
+    protected int direction = 1; // Direction of movement: 1 for positive x, -1 for negative x
 
     // Control type
-    private bool isPlayerControlled = false; // Specifies whether the unit is player-controlled or enemy-controlled
+    protected bool isPlayerControlled = false; // Specifies whether the unit is player-controlled or enemy-controlled
+
+    // Movement state
+    private bool isMoving = true; // Determines if the unit is moving
 
     // Method for taking damage
     public void TakeDamage(int damageTaken)
@@ -25,8 +28,9 @@ public abstract class AbstractUnit : MonoBehaviour
         }
     }
       
-    // Abstract method for dying (to be implemented by derived classes)
+    // Abstract methods for dying and attacking (to be implemented by derived classes)
     public abstract void Die();
+    public abstract void PlayAttackAnimationAndSound();
 
     // Method for moving the unit
     public void Move()
@@ -41,7 +45,7 @@ public abstract class AbstractUnit : MonoBehaviour
     public void setPlayerControlled()
     {
         this.isPlayerControlled = true;
-        this.direction = 1;
+        this.direction = -1;
     }
 
     public void setSpeed(int speed) 
@@ -49,4 +53,64 @@ public abstract class AbstractUnit : MonoBehaviour
         this.speed = speed; 
     }
 
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = health;
+    }
+
+    // Collision detection
+    // Collision detection
+    private void OnTriggerEnter(Collider other)
+    {
+        AbstractUnit otherUnit = other.GetComponent<AbstractUnit>();
+
+        if (otherUnit != null)
+        {
+            if (otherUnit.isPlayerControlled == this.isPlayerControlled)
+            {
+                Debug.Log($"{gameObject.name} is waiting due to collision with ally: {otherUnit.gameObject.name}");
+                isMoving = false; // Stop movement
+            }
+            else
+            {
+                Debug.Log($"{gameObject.name} is attacking enemy: {otherUnit.gameObject.name}");
+                Attack(otherUnit);
+                isMoving = false; // Stop movement during attack
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        AbstractUnit otherUnit = other.GetComponent<AbstractUnit>();
+
+        if (otherUnit != null)
+        {
+            Debug.Log($"{gameObject.name} has resumed movement after collision with: {otherUnit.gameObject.name}");
+            isMoving = true; // Resume movement
+        }
+    }
+
+    // Attack method
+    private void Attack(AbstractUnit target)
+    {
+        if (target != null)
+        {
+            target.TakeDamage(damage); // Inflict damage on the target
+
+            // Play attack animation and sound
+            PlayAttackAnimationAndSound();
+
+            // Example (commented out):
+            // Animator animator = GetComponent<Animator>();
+            // animator.Play("AttackAnimation");
+            // AudioSource audioSource = GetComponent<AudioSource>();
+            // audioSource.PlayOneShot(attackSound);
+            // animator.Play("IdleAnimation");
+        }
+    }
 }
+
